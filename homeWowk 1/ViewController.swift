9f9f475fd9be4e8da2605acc5bb49e10dcb5c547
AlinaJafar <jafarovaalinaa@gmail.com>
2021-10-01 16:11:35 +0300

    Initial Commit

---
+++ homeWowk 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWowk 1
+//
+//  Created by Alina Skopenko on 01.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
